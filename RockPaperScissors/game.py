7ed4e7a4e78f640237f4a7ed8a55e0fdd7f35ec2
GrumpyOwl <anthony.m.taormina@gmail.com>
2012-01-16 00:43:43 -0500

    Socket receiving functionality added

--- RockPaperScissors/game.py
+++ RockPaperScissors/game.py
@@ -1,1 +1,36 @@
-#testest
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("thedekel.net",1337))
+s.setblocking(0)
+
+state = 1
+queue = ""
+curr_msg = ""
+reading = False
+		
+s.send("ping")
+
+while 1:
+	try:
+		temp = s.recv(1)
+		if temp == "*":
+			reading = True
+			queue = ""
+		elif temp == "&":
+			reading = False
+			curr_msg = queue[:]
+			print curr_msg	
+		elif reading:
+			queue += temp
+	except:
+		pass	
+	if state == "0":
+		break
+	state = raw_input("Yes? ")
+
+	
+s.shutdown(socket.SHUT_RDWR)
+s.close()
+
+
